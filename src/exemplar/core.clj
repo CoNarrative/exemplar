a236b3dcdafff975acfadf96698f0b8ad3c7fdd6
Alex Dixon <alex@conarrative.com>
2017-09-26 07:42:27 -0700

    Add record, get-source, update existing tests

--- src/exemplar/core.clj
+++ src/exemplar/core.clj
@@ -11,11 +11,30 @@
         examples (read-string (if (= in "") "{}" in))]
     (spit path (with-out-str (clojure.pprint/pprint (merge examples example))))))
 
+(defn rec [lines line-number form-str]
+  (try
+    (read-string (str form-str (nth lines line-number)))
+    (catch Exception e
+      (rec lines (inc line-number) (str form-str (nth lines line-number))))))
+
+(defmacro get-source [ns name]
+  (let [met (meta (ns-resolve ns name))
+        path (:file met)
+        line (:line met)]
+    (with-open [rdr (clojure.java.io/reader path)]
+      (let [lines (line-seq rdr)]
+        `'~(rec lines (dec line) "")))))
+
 (defmacro save [sexpr]
   (let [met `(meta (var ~(first sexpr)))
-        key `(clojure.string/join "/" [(ns-name (:ns ~met)) (:name ~met)])
+        fn-ns `(ns-name (:ns ~met))
+        fn-name `(:name ~met)
+        realized-ns (eval fn-ns)
+        realized-name (eval fn-name)
+        key `(clojure.string/join "/" [~fn-ns ~fn-name])
         args (vec (rest sexpr))
-        entry `{~key {:in ~args :out ~sexpr}}]
+        source (eval `(get-source ~realized-ns ~realized-name))
+        entry `{~key {:in ~args :out ~sexpr :source (str '~source) :ns ~fn-ns :name ~fn-name}}]
     `(write-out (:path (deref exemplar.core/state)) ~entry)))
 
 (defmacro run [sym]
@@ -32,3 +51,27 @@
         examples '(read-string (slurp (:path @exemplar.core/state)))
         data `(get ~examples ~key)]
     `(merge {:name ~key} ~data)))
+
+(defn save* [ns name args ^String source out]
+  (let [key (clojure.string/join "/" [ns name])
+        entry {key {:in args :out out :source source :ns ns :name name}}]
+    (write-out (:path (deref exemplar.core/state)) entry)))
+
+(defmacro record [func]
+  `(alter-var-root (var ~func)
+    (fn [~'f]
+      (fn [& ~'args]
+        (let [met# (meta (var ~func))
+              ns# (ns-name (:ns met#))
+              name# (:name met#)]
+              ;called-form# (cons name# ~'args)]
+            (if-not (:exemplar/recording? met#)
+              (let [out# (apply ~'f ~'args)]
+                (do
+                  (alter-meta! (var ~func) assoc :exemplar/recording? true)
+                  (save* ns# name# ~'args (str (eval `(get-source ~ns# ~name#))) out#)
+                  out#))
+              (apply ~'f ~'args)))))))
+
+(defn delete-all [path]
+  (spit path "{}"))
--- test/exemplar/core_test.clj
+++ test/exemplar/core_test.clj
@@ -5,7 +5,7 @@
 (defn main-fixture [f]
   (exemplar/register-path "test.edn")
   (f)
-  (spit "test.edn" "{}"))
+  (exemplar/delete-all (:path @exemplar/state)))
 
 (use-fixtures :each main-fixture)
 
@@ -19,7 +19,9 @@
   (testing "Show after save"
     (exemplar/save (my-func [1 2 3]))
     (is (= (exemplar/show my-func)
-           {:name "exemplar.core-test/my-func"
+           {:name 'my-func
+            :ns 'exemplar.core-test
+            :source "(defn my-func [args] (map inc args))"
             :out '(2 3 4)
             :in [[1 2 3]]})))
   (testing "Run after save"
@@ -35,7 +37,9 @@
 (deftest variables
   (exemplar/save (user-ids users))
   (is (= (exemplar/show user-ids)
-         {:name "exemplar.core-test/user-ids"
+         {:name 'user-ids
+          :ns 'exemplar.core-test
+          :source "(defn user-ids [users] (mapv :id users))"
           :out [1 2]
           :in [[{:id 1 :first-name "Bob" :last-name "Smith"}
                 {:id 2 :first-name "Mary" :last-name "Brown"}]]})))
@@ -46,6 +50,8 @@
 (deftest operations-with-variables
   (exemplar/save (my-func (concat my-list my-other-list)))
   (is (= (exemplar/show my-func)
-         {:name "exemplar.core-test/my-func",
-          :out '(2 3 4 3 3 3),
+         {:name 'my-func
+          :ns 'exemplar.core-test
+          :source "(defn my-func [args] (map inc args))"
+          :out '(2 3 4 3 3 3)
           :in ['(1 2 3 2 2 2)]})))
