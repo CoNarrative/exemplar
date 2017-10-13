7b1474eed1a11a1b2f651629ddc36e74f466e72d
Alex Dixon <alex@conarrative.com>
2017-10-13 12:39:17 -0700

    Use clojure 1.9.0-beta2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject exemplar "0.0.0"
-  :description "Keep data fake."
-  :url "https://bitbucket.org/conarrative/exemplar"
+  :description "Record inputs and outputs."
+  :url "https://github.com/CoNarrative/exemplar"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
                  [local-file "0.1.0"]])
