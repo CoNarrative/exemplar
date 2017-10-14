58875ccaee78a05afe2f821e7ec19c94ea0beec8
Alex Dixon <alex@conarrative.com>
2017-10-14 07:48:29 -0700

    Use reverse domain name for clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject exemplar "0.0.0"
+(defproject com.conarrative/exemplar "0.0.0"
   :description "Record inputs and outputs."
-  :url "https://github.com/CoNarrative/exemplar"
+  :url "https://github.com/CoNarrative/exemplar.git"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0-beta2"]
-                 [local-file "0.1.0"]])
+                 [local-file "0.1.0"]]
+  :deploy-repositories [["releases"  {:sign-releases false
+                                      :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false
+                                      :url "https://clojars.org/repo"}]])
