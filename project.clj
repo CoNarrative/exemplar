50f544df4b8e6ffa36cefb1b0759f11406928bdc
Alex Dixon <alex@conarrative.com>
2017-10-14 07:50:04 -0700

    0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.conarrative/exemplar "0.0.0"
+(defproject com.conarrative/exemplar "0.0.1"
   :description "Record inputs and outputs."
   :url "https://github.com/CoNarrative/exemplar.git"
   :license {:name "Apache 2.0"
