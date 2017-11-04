624005b920d234ee025a719420bacb4c55deb614
Alex Dixon <alex@conarrative.com>
2017-11-04 12:51:15 -0700

    Add test coverage reporting

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0-beta2"]
                  [local-file "0.1.0"]]
+  :plugins [[lein-cloverage "1.0.10"]]
   :deploy-repositories [["releases"  {:sign-releases false
                                       :url "https://clojars.org/repo"}]
                         ["snapshots" {:sign-releases false
