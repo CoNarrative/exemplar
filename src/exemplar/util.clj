da1853408add96c5747e1950e0abb0fc3b49a2f1
Alex Dixon <alex@conarrative.com>
2017-11-12 08:14:55 -0800

    Update generated test if already exists

--- src/exemplar/util.clj
+++ src/exemplar/util.clj
@@ -54,7 +54,6 @@
               (if form
                 ;; Parsed target form. Write result of calling f and recur with form-read? true
                 (do
-                  (println "Got form " form line-number)
                   (doto wrt (.write (str (f form))) (.newLine))
                   (recur -1 nil true))
                 ;; Form must be on multiple lines, so recur with lines we've read so far
@@ -81,3 +80,23 @@
     (if-let [exists? (boolean (some #{[ns]} (first require-statement)))]
       form
       `(~@ns-decl ~(concat (first require-statement) [[ns]])))))
+
+(defn apply-to-forms-in-file [filepath f]
+  (with-open [rdr (clojure.java.io/reader filepath)]
+    (with-open [wrt (clojure.java.io/writer (str filepath ".tmp"))]
+      (loop [acc-form-str ""]
+        (if-let [line (.readLine rdr)]
+          ;; try to read form when line we're on is at or above where we want to start and haven't read the form
+          (let [lines (str acc-form-str line)
+                form (try (clojure.edn/read-string lines) (catch Exception e nil))]
+            (if-not form
+              ;; Form must be on multiple lines, so recur with lines we've read so far
+              (recur lines)
+              ;; Parsed a form. Write application of f if not nil. Recur with empty form buffer
+              (let [new-form (str (f form))]
+                (when new-form
+                  (doto wrt (.write new-form) (.newLine)))
+                (recur ""))))))))
+  (.renameTo
+    (clojure.java.io/file (str filepath ".tmp"))
+    (clojure.java.io/file filepath)))
--- test/exemplar/my_generated_test_file.clj
+++ test/exemplar/my_generated_test_file.clj
@@ -1,6 +1,5 @@
 (ns exemplar.my-generated-test-file (:require [clojure.test :refer [deftest is testing]] [exemplar.core]))
- 
- 
+
 (deftest exemplar-core-my-func-test
   (is (= (apply exemplar.core/my-func [[1 2 3]])
          '(1))))
