012e31cda1f7c6eb30bc1f5082aab40c60727821
Alex Dixon <alex@conarrative.com>
2018-05-14 22:25:38 -0700

    Upgrade Clojure to 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/CoNarrative/exemplar.git"
   :license {:name "Apache 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [local-file "0.1.0"]]
   :plugins [[lein-cloverage "1.0.10"]]
   :source-paths ["src"]
